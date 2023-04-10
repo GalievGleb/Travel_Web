5d99d97403f4eb0785cf02b27691f637e3c399b4
Gleb AQA <galievgleb99@gmail.com>
2023-04-10 11:31:08 +0300

    test1

---
+++ main_project/pages/login_page.py
@@ -0,0 +1,25 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class Login_page():
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    def authorization(self, login_name, login_password):
+        user_name = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, "//input[@id='user-name']")))  # обращение к элемену через id
+        user_name.send_keys(login_name)
+        print("Input Login")
+
+        password = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, "//input[@id='password']")))
+        password.send_keys(login_password)
+        print("Input Password")
+
+        button_login = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, "//input[@id='login-button']")))
+        button_login.click()
+        print("Click Login Button")
---
+++ main_project/tests/test_1.py
@@ -0,0 +1,73 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class Test_1():  # создаем класс Test_1
+    def test_select_product(self):  # создаем метод
+        driver = webdriver.Chrome(
+            executable_path='C:\\Users\\user\\Desktop\\PycharmProjects\\resource\\chromedriver.exe')  # Указали директорию в которой находится хром драйвер, для того чтобы мы могли обращаться к браузеру гугл хром
+        base_url = 'http://localhost:5173/'  # совершенствуем код в переменную пихаем юрл который используем, удобно для ооп и для списков юрл
+        driver.get(base_url)  # указываем url на который мы хотим заходить
+        driver.maximize_window()  # открывается браузер на максимальный экран
+        time.sleep(1)
+
+        print("Start Test")
+
+        sum_money = "3333"
+        a_park = "A park"
+
+        user_name = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, "//input[@type='number']")))  # обращение к элемену через id
+        user_name.send_keys(sum_money)
+        print("Input sum money")
+
+        button_login = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, "//button[@class='sc-kFuwaP gMMzMJ']")))
+        button_login.click()
+        print("Click Next Button")
+
+        button_login = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, "//div[@class='sc-csCMJt gWXRFM']")))
+        button_login.click()
+        print("Click Moscow")
+
+        button_login = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, "//button[@class='sc-kFuwaP gMMzMJ']")))
+        button_login.click()
+        print("Click Next Button")
+
+        user_name = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, "//input[@placeholder='Enter tags you entered in (search...)']")))
+        user_name.send_keys(a_park)
+        print("Input a park")
+
+        time.sleep(2)
+
+        button_login = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, "//div[@class='sc-csCMJt gWXRFM' and contains(text(), 'Amusement park')]")))
+        button_login.click()
+        print("Click Amusement park")
+
+        time.sleep(2)
+
+        button_login = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, "//button[@class='sc-kFuwaP gMMzMJ']")))
+        button_login.click()
+        print("Click Next Button")
+
+        time.sleep(10)
+
+        # succes_test = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//span[@class='title']")))
+        # value_succes_test = succes_test.text
+        # assert value_succes_test == "YOUR CART"
+        # print("Test Succes!!!!")
+        #
+        # time.sleep(5)
+
+
+test = Test_1()  # объявляем экземпляр класса Test_1
+test.test_select_product()
