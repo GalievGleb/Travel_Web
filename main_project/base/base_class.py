f00894854ababec5a9dd9a1b41434f4f5af8a203
GlebAQA <galievgleb99@gmail.com>
2023-05-08 15:55:30 +0300

    rename way screen

--- main_project/base/base_class.py
+++ main_project/base/base_class.py
@@ -22,7 +22,7 @@ class Base():
     def get_screenshot(self):
         now_date = datetime.datetime.utcnow().strftime("%Y.%m.%d.%H.%M")
         name_screenshot = 'screenshot' + now_date + '.png'
-        self.driver.save_screenshot('C:\\Users\\galie\\Desktop\\travel_web\\screen' + name_screenshot)
+        self.driver.save_screenshot('C:\\Users\\galie\\Desktop\\travel_web\\screen\\' + name_screenshot)
 
     """Method assert word"""
 
